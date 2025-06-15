c3a872620590b571db5776f939bef384100b4d92
Yauheni Shumsky <shumsky.yauheni@gmail.com>
2025-06-15 21:52:26 +0300

    Add repositories in DI

--- Coursework.WebAPI/Program.cs
+++ Coursework.WebAPI/Program.cs
@@ -1,4 +1,6 @@
+using Coursework.Domain.Interfaces.Repositories;
 using Coursework.Infrastructure;
+using Coursework.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +11,22 @@ builder.Services.AddDbContext<CourseworkDbContext>(options =>
         options.UseSqlServer(configuration.GetConnectionString(nameof(CourseworkDbContext)));
     }
     );
+
+builder.Services.AddScoped<IAnswerRepository, AnswerRepository>();
+
+builder.Services.AddScoped<ICommentRepository, CommentRepository>();
+
+builder.Services.AddScoped<IFormRepository, FormRepository>();
+
+builder.Services.AddScoped<ILikeRepository, LikeRepository>();
+
+builder.Services.AddScoped<IQuestionRepository, QuestionRepository>();
+
+builder.Services.AddScoped<ITagRepository, TagRepository>();
+
+builder.Services.AddScoped<ITemplateRepository, TemplateRepository>();
+
+builder.Services.AddScoped<IUserRepository, UserRepository>();
 
 var app = builder.Build();
 
