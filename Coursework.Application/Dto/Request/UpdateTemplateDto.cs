de6ac4fe207af36dcb48c48eed127e600d47c71b
Yauheni Shumsky <shumsky.yauheni@gmail.com>
2025-06-17 01:31:53 +0300

    refactoring update template dto

--- Coursework.Application/Dto/Request/UpdateTemplateDto.cs
+++ Coursework.Application/Dto/Request/UpdateTemplateDto.cs
@@ -6,5 +6,4 @@ public class UpdateTemplateDto
     public string Description { get; set; } = string.Empty;
     public List<string> Images { get; set; } = [];
     public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
-    public List<string> Tags { get; set; } = [];
 }
