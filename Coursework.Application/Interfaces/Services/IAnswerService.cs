baae2b8af29e515d9581c6ee5c889bbefe88f46d
Yauheni Shumsky <shumsky.yauheni@gmail.com>
2025-06-19 02:04:59 +0300

    refactoring answer

--- Coursework.Application/Interfaces/Services/IAnswerService.cs
+++ Coursework.Application/Interfaces/Services/IAnswerService.cs
@@ -1,7 +1,4 @@
-using Coursework.Application.Dto.Request.AddDtos;
-using Coursework.Application.Dto.Request.UpdateDtos;
 using Coursework.Application.Dto.Response;
-using Coursework.Domain.Models;
 
 namespace Coursework.Application.Interfaces.Services;
 
@@ -9,8 +6,4 @@ public interface IAnswerService
 {
     public Task<List<GetAnswerDto>> GetAllByForm(uint formId);
     public Task<GetAnswerDto> GetById(uint id);
-    public Task<Answer> GetByIdWithoutMapping(uint id);
-    public Task Create(AddAnswerDto answer, uint questionId);
-    public Task Update(UpdateAnswerDto newValue, uint id);
-    public Task Delete(uint id);
 }
